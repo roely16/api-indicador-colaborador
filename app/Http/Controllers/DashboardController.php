eda77397e415ee99dd8edfb943d0e1a8e78c9267
Roely Chur <gerson.roely@gmail.com>
2021-03-25 11:24:37 -0600

    Indicador Performance

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -87,6 +87,18 @@
 
                         //$criterio->pendiente = true;
 
+                        $data = [
+                            "colaborador" => $empleado,
+                            "criterio" => $criterio
+                        ];
+
+                        $result = $this->{$criterio->funcion_calculo}($data);
+
+                        $criterio->color = $result["color"];
+                        $criterio->calificacion = $result["calificacion"];
+
+                        //return response()->json($result);
+
                     }
                     
                 }
@@ -99,9 +111,103 @@
 
         }
         
-        public function performance($colaborador){
+        public function performance($data){
 
+            $colaborador = $data["colaborador"];
 
+            // Obtener los porcentaje de evaluación
+            $result = app('db')->select("   SELECT *
+                                            FROM RH_COLABORADORES_TEMPORADA
+                                            WHERE NIT = '$colaborador->nit'
+                                            ORDER BY TEMPORADAID DESC");
+            
+            if ($result) {
+                
+                $colaborador->porcentaje_colega = $result[0]->porcentaje_colega;
+                $colaborador->porcentaje_asesor = $result[0]->porcentaje_asesor;
+                $colaborador->temporadaid = $result[0]->temporadaid;
+
+            }
+
+            // Por cada colaborador buscar la evaluación de superior
+            $evaluacion_superior = app('db')->select("  SELECT *
+                                                        FROM RH_RESULTADOS_TEMPORADA
+                                                        WHERE TEMPORADAID = '$colaborador->temporadaid'
+                                                        AND NIT = '$colaborador->nit'
+                                                        AND TIPOEV = 'superior'");
+
+            $colaborador->superior = $evaluacion_superior;
+
+            // Obtener el promedio de calificacion
+
+            $total = 0;
+
+            foreach ($evaluacion_superior as $item) {
+
+                $total += $item->calificacion;
+
+            }
+
+            if ($evaluacion_superior) {
+
+                $colaborador->superior_promedio = $total / count($evaluacion_superior);
+
+            }else{
+
+                $colaborador->superior_promedio = 0;
+
+            }
+
+            // Colega
+            $evaluacion_colega = app('db')->select("    SELECT *
+                                                        FROM RH_RESULTADOS_TEMPORADA
+                                                        WHERE TEMPORADAID = '$colaborador->temporadaid'
+                                                        AND NIT = '$colaborador->nit'
+                                                        AND TIPOEV = 'colega'");
+
+            $colaborador->colega = $evaluacion_colega;
+
+            // Obtener el promedio de calificación
+
+            $total = 0;
+
+            foreach ($evaluacion_colega as $item) {
+
+                $total += $item->calificacion;
+
+            }
+
+            if ($evaluacion_colega) {
+
+                $colaborador->colega_promedio = $total / count($evaluacion_colega);
+
+            }else{
+
+                $colaborador->colega_promedio = 0;
+
+            }
+
+            // Sacar el total de la calificación
+
+            $colaborador->calificacion = round((($colaborador->colega_promedio * intval($colaborador->porcentaje_colega)) / 100) + (($colaborador->superior_promedio * intval($colaborador->porcentaje_asesor)) / 100), 2);
+
+            // Asignar un color
+
+            if ($colaborador->calificacion >= 0 && $colaborador->calificacion < 60) {
+
+                $colaborador->color = 'red';
+
+            }elseif( $colaborador->calificacion >= 60 && $colaborador->calificacion < 80){
+
+                $colaborador->color = 'orange';
+
+            }else{
+
+                $colaborador->color = 'green';
+
+            }
+
+            return $colaborador;
 
         }
 
