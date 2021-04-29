cf3cbe85b30c18929d8858a1c8ea6ccec7304949
Roely Chur <gerson.roely@gmail.com>
2021-04-29 13:52:03 -0600

    Imagen de perfil

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -32,6 +32,28 @@
 
             $result = $result[0];
 
+            // Buscar la imagen del colaborador
+            $imagen = app('db')->select("   SELECT *
+                                            FROM RH_RUTA_PDF
+                                            WHERE NIT = '$result->nit'
+                                            AND IDCAT = '11'");
+            if ($imagen) {
+                
+                $result->imagen = $imagen[0]->ruta;
+
+                $result->imagen = $_SERVER['DOCUMENT_ROOT'] . '/GestionServicios/' . $result->imagen;
+
+                $type = pathinfo($result->imagen, PATHINFO_EXTENSION);
+                $data = file_get_contents($result->imagen);
+                $result->imagen64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+
+            }else{
+
+                $result->imagen = null;
+                $result->imagen64 = null;
+
+            }
+            
             // Buscar la sección
             $seccion = app('db')->select("  SELECT *
                                             FROM RH_AREAS
